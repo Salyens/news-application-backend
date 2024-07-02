894a3ad9137476bc16a1a7eb506fdab7c3018316
Salyens <salyens@yandex.ru>
2024-07-02 17:51:24 +0400

    deploy on Render

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ const PORT = process.env.PORT || 3000;
  */
 const allowedOrigins = [
   "http://localhost:3001",
+  "http://localhost:3000",
   "http://127.0.0.1:5500",
   "http://127.0.0.1:5501",
 ];
