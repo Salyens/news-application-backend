461d428f24bef6f4cf111bd1543a4496d8363ba1
Salyens <salyens@yandex.ru>
2024-07-03 17:25:26 +0400

    refactoring

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,11 +1,14 @@
 /**
  * Validates if the given email is in a proper format.
+ * This function uses a more comprehensive regular expression to cover a wide range of valid email formats.
  * 
  * @param {string} email - The email to validate.
  * @returns {boolean} Returns true if the email is valid, false otherwise.
  */
 const validEmail = (email) => {
-  const regex = new RegExp(/^\w+@\w{2,}\.[a-z]{2,3}$/i);
+  const regex = new RegExp(
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  );
   return regex.test(email);
 };
 
